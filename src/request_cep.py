9939cec9bf8e31b9ed83fb8e5c1d27acbc255573
Oliveira-Carlos <carloseduardoipu01@gmail.com>
2023-09-07 23:21:36 -0300

    feat: add user interaction on latitude and longitude location

--- src/request_cep.py
+++ src/request_cep.py
@@ -1,4 +1,6 @@
-import requests
+from dotenv import load_dotenv
+import os
+import psycopg2
 from geopy.geocoders import Nominatim
 
 # Função para obter as coordenadas de latitude e longitude a partir de um endereço
@@ -13,20 +15,49 @@ def get_coordinates(address):
         return None, None
 
 
-# Viacep
-cep = '54505560'
+load_dotenv()
 
-link = f'https://viacep.com.br/ws/{cep}/json/'
+dbname = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASS")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+
+# Solicitar ao usuário que insira o CEP
+cep = input("Digite o CEP: ")
+
+# Consulta SQL para obter informações do CEP
+query = f"SELECT municipio, uf FROM unidades_saude WHERE cep = '{cep}'"
 
 try:
-    req_cep = requests.get(link)
-    data = req_cep.json()
+    # Conectar ao banco de dados
+    conn = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
 
-    cidade = data.get('localidade', '')  # 'localidade' é a cidade
-    estado = data.get('uf', '')          # 'uf' é o estado
-    if cidade and estado:
-        endereco = f"{cidade}, {estado}"  # Ordem correta: "cidade, estado"
+    # Criar um cursor
+    cur = conn.cursor()
+
+    # Executar a consulta SQL
+    cur.execute(query)
+
+    # Obter os resultados da consulta
+    result = cur.fetchone()
+
+    # Verificar se o CEP foi encontrado no banco de dados
+    if result:
+        cidade, estado = result
+
+        # Construir o endereço a partir da cidade e estado
+        endereco = f"{cidade}, {estado}"
+
+        # Obter as coordenadas
         latitude, longitude = get_coordinates(endereco)
+
         if latitude is not None and longitude is not None:
             print(f"Coordenadas de {cidade}, {estado}:")
             print(f"Latitude: {latitude}")
@@ -34,6 +65,12 @@ try:
         else:
             print("Não foi possível obter as coordenadas do endereço.")
     else:
-        print("Dados incompletos na resposta da API.")
-except requests.exceptions.RequestException as e:
-    print(f"Erro de requisição: {e}")
+        print("CEP não encontrado no banco de dados.")
+
+except psycopg2.Error as e:
+    print(f"Erro ao conectar ao banco de dados: {e}")
+
+finally:
+    # Fechar a conexão com o banco de dados
+    if conn is not None:
+        conn.close()
